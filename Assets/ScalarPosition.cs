09c59d16d5e72fde8136e1da761da283ad4ae670
KevX36 <kevincard2007@gmail.com>
2025-11-03 12:01:02 -0400

    finished scene 2 (thanks charlly overhereing you help someone else also helped me)

--- Assets/ScalarPosition.cs
+++ Assets/ScalarPosition.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 public class ScalarPosition : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
-        
+        startPos = transform.position;
+        transform.position = new Vector3(0, 0, 0);
     }
-
+    public float step;
+    private float scalar = 1;
+    Vector3 startPos;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            scalar += step;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            scalar -= step;
+        }
+        transform.position = (startPos * scalar);
     }
 }
