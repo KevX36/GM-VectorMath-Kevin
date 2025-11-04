5dd200041111c62dd8416f88426dae92bfd9b736
KevX36 <kevincard2007@gmail.com>
2025-11-04 11:34:00 -0400

    just gotta add the dot

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -7,21 +7,25 @@ public class playerScript : MonoBehaviour
     void Start()
     {
         Pos = transform.position;
-        forward.transform.position = Vector3.forward;
+        forward.transform.position = Pos + Vector3.forward;
+        
     }
     Vector3 Pos;
     public GameObject forward;
+    public GameObject enemy;
+    Vector3 enemyDir;
+    private float playerDotEnemyDir;
     // Update is called once per frame
     void Update()
     {
-        
+        enemyDir = enemy.transform.position;
         if (Input.GetKeyDown(KeyCode.W))
         {
-            Pos.y++;
+            Pos.z++;
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Pos.y--;
+            Pos.z--;
         }
         transform.position = Pos;
     }
